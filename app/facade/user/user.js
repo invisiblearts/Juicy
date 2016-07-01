9676385f0d7732c4470c99443410cb5a22e5be1e
POJO <octetrmx@gmail.com>
2016-07-02 01:51:23 +0800

    user login fix ( still corrupted

--- app/facade/user/user.js
+++ app/facade/user/user.js
@@ -4,11 +4,13 @@ var User = require('../../domain/user/model');
 var bcrypt = require('bcrypt')
 var jwt = require('jsonwebtoken');
 var fs = require('fs');
-var cert = fs.readFileSync('private.key');
 
 function userRest(api){
     api.post('/login', function(req, res) {
-        var u = User.findOne({'username':req.body.username}).exec().then(r=>{
+         User.findOne({'username':req.body.username}).exec().then(r=>{
+             if(!r.password){
+            res.sendStatus(403);
+        }
                 bcrypt.compare(req.body.password, r.password,function(err,authorized){
                 if(authorized){
                     res.json(jwt.sign({id: user._id,isAdmin: user.isAdmin}, 'shhhhh'));
