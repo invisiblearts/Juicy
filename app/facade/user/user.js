8ae66617a32dfd0d6dd643fbf3c1154aa3ad8956
ThrowableFactory <ThrowableFactory@foxmail.com>
2016-07-02 14:14:19 +0800

    ok

--- app/facade/user/user.js
+++ app/facade/user/user.js
@@ -3,35 +3,34 @@ var Promise = require('bluebird');
 var User = require('../../domain/user/model');
 var bcrypt = require('bcrypt')
 var jwt = require('jsonwebtoken');
-var fs = require('fs');
+var key = require('../../key');
 
 function userRest(api){
     api.post('/login', function(req, res) {
-         User.find({'username':req.body.username}).exec().then(r=>{
-             if(!r)
-        {
-            return res.sendStatus(403);
-
-        }
-                bcrypt.compare(req.body.password, r.password,function(err,authorized){
-                if(authorized){
-                    res.json(jwt.sign({id: user._id,isAdmin: user.isAdmin}, 'shhhhh'));
-                }
-            })
-
-
-    })});
+         User.findOne({'username':req.body.username}).exec().then(r=>{
+           if(!r) {
+             return res.sendStatus(403);
+           }
+           bcrypt.compare(req.body.password, r.password,function(err,authorized){
+             if(authorized){
+               res.json(jwt.sign({id: r._id,isAdmin: r.isAdmin}, key))
+             }else{
+               res.json({});
+             }
+           })
+         })
+    })
 
     api.post('/reg', function(req, res) {
         var u = new User();
         u.username = req.body.username;
-        bcrypt.hash(req.body.password, 'shhhhh', function(err, hash) {
+        bcrypt.hash(req.body.password, 12, function(err, hash) {
             u.password = hash;
             u.isAdmin = false;
             u.save(
                 (err, user, numAffected)=>
                 {
-                    res.json(jwt.sign({id: u._id,isAdmin: u.isAdmin}, 'shhhhh'));
+                    res.json(jwt.sign({id: u._id,isAdmin: u.isAdmin}, key));
 
                 });
         });
