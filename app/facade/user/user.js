2c630d614dcbc563a00d6cab851d130c19757afb
POJO <octetrmx@gmail.com>
2016-07-02 13:39:25 +0800

    user login fix ( still corrupted

--- app/facade/user/user.js
+++ app/facade/user/user.js
@@ -7,18 +7,21 @@ var fs = require('fs');
 
 function userRest(api){
     api.post('/login', function(req, res) {
-         User.findOne({'username':req.body.username}).exec().then(r=>{
-             if(!r.password){
-            res.sendStatus(403);
+         User.find({'username':req.body.username}).exec(r=>{
+             if(!r)
+        {
+            return res.sendStatus(403);
+
         }
                 bcrypt.compare(req.body.password, r.password,function(err,authorized){
                 if(authorized){
                     res.json(jwt.sign({id: user._id,isAdmin: user.isAdmin}, 'shhhhh'));
                 }
             })
-            });
 
-    });
+
+    })});
+
     api.post('/reg', function(req, res) {
         var u = new User();
         u.username = req.body.username;
@@ -28,7 +31,7 @@ function userRest(api){
             u.save(
                 (err, user, numAffected)=>
                 {
-                    res.json(jwt.sign({id: user._id,isAdmin: user.isAdmin}, 'shhhhh'));
+                    res.json(jwt.sign({id: u._id,isAdmin: u.isAdmin}, 'shhhhh'));
 
                 });
         });
