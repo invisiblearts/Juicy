5857213455ac102a4cadc10a28f1f592c13bad55
POJO <octetrmx@gmail.com>
2016-07-04 00:40:34 +0800

    done

--- app/domain/beat/assembler.js
+++ app/domain/beat/assembler.js
@@ -4,20 +4,20 @@ function BeatAssembler(){
     return {
         addYYMM : addYYMM,
         dateToYYMM : dateToYYMM
-    }
+    };
+    
     function addYYMM(beats){
         var res = {};
         res.beats = beats;
         if(beats.length && beats[0]){
-            date = new Date(beats[0].time);
+            var date = new Date(beats[0].time);
             res.time = Util.genYYMMByDate(date);
         }
         return res;
     }
 
     function dateToYYMM(data){
-        var res = data.map(i=>Util.genYYMM(i._id.year,i._id.month));
-        return res;
+        return data.map(i=>Util.genYYMM(i._id.year,i._id.month));
     }
 }
 module.exports = BeatAssembler();
