0b7d264f8ff8a96a9a9b85783588345c670f98f4
ThrowableFactory <ThrowableFactory@foxmail.com>
2016-07-30 18:22:13 +0800

    modified styles

--- app/web/src/app/app.const.config.js
+++ app/web/src/app/app.const.config.js
@@ -2,7 +2,7 @@
 
   angular.module('app')
   .constant('APP_CONST', {
-    title: 'src | moe',
+    title: 'src.moe',
     version: 'v2 alpha5',
     production: true,
     api: 'https://src.moe/api/'
